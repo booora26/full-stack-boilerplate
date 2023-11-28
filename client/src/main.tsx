6abe4cc6fc7498ba85d242c2655a7155c101854f
boris <bgluvacevic@gmail.com>
2023-11-28 18:11:46 +0100

    main change

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -7,18 +7,14 @@ import { LogInPage } from "./pages/auth/LogInPage";
 import { RegistrationPage } from "./pages/auth/RegistrationPage";
 import { TwoFAPage } from "./pages/auth/TwoFAPage";
 import {
-  MainContext,
   UserContext,
-  unAuthenticatedUser,
 } from "./context/AuthContext.tsx";
 import { AuthenticatedPage } from "./pages/auth/AuthenticatedPage.tsx";
 import { UsersPage, loader as usersLoader } from "./pages/UsersPage.tsx";
-import { ResetPassword } from "./components/ResetPassword.tsx";
 import { ResetPasswordPage } from "./pages/auth/ResetPasswordPage.tsx";
 import "./assets/main.css";
 import { RootLayout } from "./layout/RootLayout.tsx";
 import { UserDetails } from "./components/UserDetails.tsx";
-import { Details } from "./components/Details.tsx";
 
 const router = createBrowserRouter([
   {
