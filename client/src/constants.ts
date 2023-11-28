b7a9bf8e4b8e0f8fda5ac193899fa1101ed1405c
boris <bgluvacevic@gmail.com>
2023-11-28 17:53:34 +0100

    env change

--- client/src/constants.ts
+++ client/src/constants.ts
@@ -1,2 +1,2 @@
-export const CLIENT_URL = 'http://localhost:3010'
-export const SERVER_URL = 'http://localhost:4010'
+export const CLIENT_URL = 'http://46.101.119.174:3010'
+export const SERVER_URL = 'http://46.101.119.174:4010'
