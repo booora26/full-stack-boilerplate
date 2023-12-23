dae36b85daf7ba2cefbf259a03fb2f02eddd3ddb
boris <bgluvacevic@gmail.com>
2023-12-23 18:05:05 +0100

    Add console log statement to getFreeAppointmentByEmployee function

--- client/src/api/appointements.ts
+++ client/src/api/appointements.ts
@@ -15,6 +15,8 @@ export const getFreeAppointmentByEmployee = async () => {
     const response = await fetch(`${serverURL}/appointment/free-by-emp`, {method: 'POST',     headers: myHeaders,
     credentials: 'include', body: JSON.stringify({shop: {"id": 1}, employee: {"id": 1}, date: '2023-12-01', service: {"id": 1}})});
     const status = await response.json();
+
+    console.log('fetch', status);
   
     return status;
   };
